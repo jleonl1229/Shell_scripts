9fbbd4825309272adcdb4d663ce090cf626ddac3
Debian Live user <user@debian-BULLSEYE-live-builder-AMD64>
2024-05-11 18:09:14 +0000

    Better now

--- vimcracker/vimcracker.sh
+++ vimcracker/vimcracker.sh
@@ -11,6 +11,9 @@ do
 		xdtool key Return
 		if [xdotool type Return -eq 0];
 		then
+			xdotool key shift+semicolon
+			xdotool type ':q!'
+			echo "The password was $line"	
 			break
 		else
 			xdotool key shift+semicolon
