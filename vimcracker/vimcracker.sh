11a5a927e2a04e18ac4e8e5f2e6fabd90af439a8
Debian Live user <user@debian-BULLSEYE-live-builder-AMD64>
2024-05-11 17:03:41 +0000

    Starting out with folders program

---
+++ vimcracker/vimcracker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while read line;
+do
+	vim testing.txt
+	if [$? -eq 0];
+	then
+		xdotool key Return
+		sleep 1
+		xdotool type '$line'
+		xdtool key Return
+		if [xdotool type Return -eq 0];
+		then
+			break
+		else
+			xdotool key shift+semicolon
+			xdotool type ':q!'
+			xdotool type Return
+		fi
+	else
+		echo "No file found"
+	fi
+done < $1
