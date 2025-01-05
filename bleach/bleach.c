4a3424b55e68f6af157e07d98dac040b0c7d4bc7
Julen Leon Larrondo <jleon-la@c4r8s6.42urduliz.com>
2025-01-05 15:54:41 +0100

    C version finished

--- bleach/bleach.c
+++ bleach/bleach.c
@@ -2,30 +2,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h> 
-#define BUFFER_SIZE 99999
 
 int main(int ac, char **av)
 {
-	int	i;
-	char	*buffer;
-	int	bytes_read;
+	long	i;
+	off_t	total;
 	int 	fd;
 
 	if (ac != 2)
-		return(write(1, "File name required\n", 19), 1);
+		return(write(2, "File name required\n", 19), 1);
 	fd = open(av[1], O_RDWR);
 	if (fd == -1)
-		return(write(1, "File not found\n", 15), 1);
-	buffer = malloc(sizeof(char) * BUFFER_SIZE);
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
+		return(write(2, "File not found\n", 15), 1);
+	lseek(fd, 0, SEEK_END);
+	total = lseek(fd, 0, SEEK_CUR);
+	if (lseek(fd, 0, SEEK_SET) == -1)
+		return (write(2, "Error", 5), 1);
 	i = 0;
-	if (lseek(fd, i, SEEK_SET) == -1)
-		return (write(1, "Error", 5), 1);
-	while (i < bytes_read)
+	while (i < total)
 	{
 		write(fd, "\0", 1);
 		i++;
 	}
-	printf("%d\n", bytes_read);
-	return (write(fd, "\n", 1), free(buffer), close(fd), 0);
+	printf("Total bytes in the file: %ld | Overrided: %ld\n", total, i);
+	return (write(2, "\n", 1), close(fd), 0);
 }
